19b82b5d5e9276ba85237c7afb7a284bc3888805
Fazlul <fkarif.cse@gmail.com>
2019-10-23 01:36:57 +0200

    structure the componets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import CalButtons from "./components/CalButtons";
+import SingleButton from "./components/SingleButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      operations: []
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <CalButtons>
+            <SingleButton>7</SingleButton>
+          </CalButtons>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SingleButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleButton = props => {
+  return <div>{props.children}</div>;
+};
+
+export default SingleButton;
