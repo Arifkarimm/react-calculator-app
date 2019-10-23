905bdc0020727e042d5985b344ad52ffacb072e1
Fazlul <fkarif.cse@gmail.com>
2019-10-23 02:06:12 +0200

    calculate function fix and initila addition workd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import update from "immutability-helper";
+import * as math from "mathjs";
 import "./App.css";
 import CalButtons from "./components/CalButtons";
 import SingleButton from "./components/SingleButton";
+import DisplayResult from "./components/DisplayResult";
 
 class App extends Component {
   constructor(props) {
@@ -14,13 +17,37 @@ class App extends Component {
     this.onHandleClick = this.onHandleClick.bind(this);
   }
 
-  onHandleClick() {
-    console.log("success");
+  calculatorCalculation = event => {
+    let result = this.state.operations.join("");
+    if (result) {
+      result = eval(result);
+      result = math.format(result, { precision: 14 });
+      this.setState({
+        operations: [result]
+      });
+    }
+  };
+
+  onHandleClick(event) {
+    const value = event.target.getAttribute("value");
+    switch (value) {
+      case "equal":
+        this.calculatorCalculation();
+        break;
+
+      default:
+        const newOperations = update(this.state.operations, { $push: [value] });
+        this.setState({
+          operations: newOperations
+        });
+        break;
+    }
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <DisplayResult>{this.state.operations}</DisplayResult>
           <CalButtons>
             <SingleButton
               onClick={this.onHandleClick}
@@ -31,6 +58,11 @@ class App extends Component {
               onClick={this.onHandleClick}
               label="4"
               value="4"
+            ></SingleButton>
+            <SingleButton
+              onClick={this.onHandleClick}
+              label="+"
+              value="+"
             ></SingleButton>
             <SingleButton
               onClick={this.onHandleClick}
